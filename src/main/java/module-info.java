8d3ac85aebb00b38213dcf36021e19e493ea92b2
AaronI45 <zs20015694@estudiantes.uv.mx>
2023-11-02 04:22:13 -0600

    iniciar repositorio

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module secondlibrary.secondlibrary {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens secondlibrary.main to javafx.fxml;
+    exports secondlibrary.main;
+}
